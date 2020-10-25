7fdf0ef98baef642be02da7bc1f8ae4dbf9ee227
nk2303 <ngankhong03@gmail.com>
2020-10-24 19:20:20 -0700

    formatting

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-
 // eslint-disable-next-line no-unused-vars
-const projectName = 'javascript-calculator';
-
-
-// coded by @no-stack-dub-sack (github) / @no_stack_sub_sack (codepen)
+const projectName = "javascript-calculator";
 
 // VARS:
 const isOperator = /[x/+‑]/,
-endsWithOperator = /[x+‑/]$/,
-endsWithNegativeSign = /[x/+]‑$/,
-clearStyle = { background: '#F57D7C' },
-operatorStyle = { background: '#5A809E' },
-equalsStyle = {
-  background: '#6CC2BD',
-  position: 'absolute',
-  height: 130,
-  bottom: 5 };
-  
+  endsWithOperator = /[x+‑/]$/,
+  endsWithNegativeSign = /[x/+]‑$/,
+  clearStyle = { background: "#F57D7C" },
+  operatorStyle = { background: "#5A809E" },
+  equalsStyle = {
+    background: "#6CC2BD",
+    position: "absolute",
+    height: 130,
+    bottom: 5,
+  };
 
 // COMPONENTS:
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentVal: '0',
-      prevVal: '0',
-      formula: '',
-      currentSign: 'pos',
-      lastClicked: '' };
+      currentVal: "0",
+      prevVal: "0",
+      formula: "",
+      currentSign: "pos",
+      lastClicked: "",
+    };
 
     this.maxDigitWarning = this.maxDigitWarning.bind(this);
     this.handleOperators = this.handleOperators.bind(this);
@@ -39,32 +36,33 @@ class Calculator extends React.Component {
 
   maxDigitWarning() {
     this.setState({
-      currentVal: 'Digit Limit Met',
-      prevVal: this.state.currentVal });
+      currentVal: "Digit Limit Met",
+      prevVal: this.state.currentVal,
+    });
 
     setTimeout(() => this.setState({ currentVal: this.state.prevVal }), 1000);
   }
 
   handleEvaluate() {
-    if (!this.state.currentVal.includes('Limit')) {
+    if (!this.state.currentVal.includes("Limit")) {
       let expression = this.state.formula;
       while (endsWithOperator.test(expression)) {
         expression = expression.slice(0, -1);
       }
-      expression = expression.replace(/x/g, '*').replace(/‑/g, '-');
+      expression = expression.replace(/x/g, "*").replace(/‑/g, "-");
       let answer = Math.round(1000000000000 * eval(expression)) / 1000000000000;
       this.setState({
         currentVal: answer.toString(),
         formula:
-        expression.replace(/\*/g, '⋅').replace(/-/g, '‑') + '=' + answer,
+          expression.replace(/\*/g, "⋅").replace(/-/g, "‑") + "=" + answer,
         prevVal: answer,
-        evaluated: true });
-
+        evaluated: true,
+      });
     }
   }
 
   handleOperators(e) {
-    if (!this.state.currentVal.includes('Limit')) {
+    if (!this.state.currentVal.includes("Limit")) {
       const value = e.target.value;
       const { formula, prevVal, evaluated } = this.state;
       this.setState({ currentVal: value, evaluated: false });
@@ -73,23 +71,24 @@ class Calculator extends React.Component {
       } else if (!endsWithOperator.test(formula)) {
         this.setState({
           prevVal: formula,
-          formula: formula + value });
-
+          formula: formula + value,
+        });
       } else if (!endsWithNegativeSign.test(formula)) {
         this.setState({
-          formula: (endsWithNegativeSign.test(formula + value) ?
-          formula : prevVal) + value });
-
-      } else if (value !== '‑') {
+          formula:
+            (endsWithNegativeSign.test(formula + value) ? formula : prevVal) +
+            value,
+        });
+      } else if (value !== "‑") {
         this.setState({
-          formula: prevVal + value });
-
+          formula: prevVal + value,
+        });
       }
     }
   }
 
   handleNumbers(e) {
-    if (!this.state.currentVal.includes('Limit')) {
+    if (!this.state.currentVal.includes("Limit")) {
       const { currentVal, formula, evaluated } = this.state;
       const value = e.target.value;
       this.setState({ evaluated: false });
@@ -98,21 +97,23 @@ class Calculator extends React.Component {
       } else if (evaluated) {
         this.setState({
           currentVal: value,
-          formula: value !== '0' ? value : '' });
-
+          formula: value !== "0" ? value : "",
+        });
       } else {
         this.setState({
           currentVal:
-          currentVal === '0' || isOperator.test(currentVal) ?
-          value :
-          currentVal + value,
+            currentVal === "0" || isOperator.test(currentVal)
+              ? value
+              : currentVal + value,
           formula:
-          currentVal === '0' && value === '0' ?
-          formula === '' ? value : formula :
-          /([^.0-9]0|^0)$/.test(formula) ?
-          formula.slice(0, -1) + value :
-          formula + value });
-
+            currentVal === "0" && value === "0"
+              ? formula === ""
+                ? value
+                : formula
+              : /([^.0-9]0|^0)$/.test(formula)
+              ? formula.slice(0, -1) + value
+              : formula + value,
+        });
       }
     }
   }
@@ -120,181 +121,248 @@ class Calculator extends React.Component {
   handleDecimal() {
     if (this.state.evaluated === true) {
       this.setState({
-        currentVal: '0.',
-        formula: '0.',
-        evaluated: false });
-
+        currentVal: "0.",
+        formula: "0.",
+        evaluated: false,
+      });
     } else if (
-    !this.state.currentVal.includes('.') &&
-    !this.state.currentVal.includes('Limit'))
-    {
+      !this.state.currentVal.includes(".") &&
+      !this.state.currentVal.includes("Limit")
+    ) {
       this.setState({ evaluated: false });
       if (this.state.currentVal.length > 21) {
         this.maxDigitWarning();
       } else if (
-      endsWithOperator.test(this.state.formula) ||
-      this.state.currentVal === '0' && this.state.formula === '')
-      {
+        endsWithOperator.test(this.state.formula) ||
+        (this.state.currentVal === "0" && this.state.formula === "")
+      ) {
         this.setState({
-          currentVal: '0.',
-          formula: this.state.formula + '0.' });
-
+          currentVal: "0.",
+          formula: this.state.formula + "0.",
+        });
       } else {
         this.setState({
-          currentVal: this.state.formula.match(/(-?\d+\.?\d*)$/)[0] + '.',
-          formula: this.state.formula + '.' });
-
+          currentVal: this.state.formula.match(/(-?\d+\.?\d*)$/)[0] + ".",
+          formula: this.state.formula + ".",
+        });
       }
     }
   }
 
   initialize() {
     this.setState({
-      currentVal: '0',
-      prevVal: '0',
-      formula: '',
-      currentSign: 'pos',
-      lastClicked: '',
-      evaluated: false });
-
+      currentVal: "0",
+      prevVal: "0",
+      formula: "",
+      currentSign: "pos",
+      lastClicked: "",
+      evaluated: false,
+    });
   }
 
   render() {
-    return (
-      React.createElement("div", null,
-      React.createElement("div", { className: "calculator" },
-      React.createElement(Formula, { formula: this.state.formula.replace(/x/g, '⋅') }),
-      React.createElement(Output, { currentValue: this.state.currentVal }),
-      React.createElement(Buttons, {
-        decimal: this.handleDecimal,
-        evaluate: this.handleEvaluate,
-        initialize: this.initialize,
-        numbers: this.handleNumbers,
-        operators: this.handleOperators })),
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "div",
+        { className: "calculator" },
+        React.createElement(Formula, {
+          formula: this.state.formula.replace(/x/g, "⋅"),
+        }),
+        React.createElement(Output, { currentValue: this.state.currentVal }),
+        React.createElement(Buttons, {
+          decimal: this.handleDecimal,
+          evaluate: this.handleEvaluate,
+          initialize: this.initialize,
+          numbers: this.handleNumbers,
+          operators: this.handleOperators,
+        })
+      ),
 
-
-      React.createElement("div", { className: "author" },
-      ' ', "Redesigned and Coded By ",
-      React.createElement("br", null),
-      React.createElement("a", { href: "https://github.com/nk2303/calculator-js", target: "_blank" }, "Ngan Kim Khong"))));
-
-
-
-
-
-  }}
-
+      React.createElement(
+        "div",
+        { className: "author" },
+        " ",
+        "Redesigned and Coded By ",
+        React.createElement("br", null),
+        React.createElement(
+          "a",
+          { href: "https://github.com/nk2303/calculator-js", target: "_blank" },
+          "Ngan Kim Khong"
+        )
+      )
+    );
+  }
+}
 
 class Buttons extends React.Component {
   render() {
-    return (
-      React.createElement("div", null,
-      React.createElement("button", {
-        className: "jumbo",
-        id: "clear",
-        onClick: this.props.initialize,
-        style: clearStyle,
-        value: "AC" }, "AC"),
+    return React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "button",
+        {
+          className: "jumbo",
+          id: "clear",
+          onClick: this.props.initialize,
+          style: clearStyle,
+          value: "AC",
+        },
+        "AC"
+      ),
 
+      React.createElement(
+        "button",
+        {
+          id: "divide",
+          onClick: this.props.operators,
+          style: operatorStyle,
+          value: "/",
+        },
+        "/"
+      ),
 
+      React.createElement(
+        "button",
+        {
+          id: "multiply",
+          onClick: this.props.operators,
+          style: operatorStyle,
+          value: "x",
+        },
+        "x"
+      ),
 
-      React.createElement("button", {
-        id: "divide",
-        onClick: this.props.operators,
-        style: operatorStyle,
-        value: "/" }, "/"),
+      React.createElement(
+        "button",
+        { id: "seven", onClick: this.props.numbers, value: "7" },
+        "7"
+      ),
 
+      React.createElement(
+        "button",
+        { id: "eight", onClick: this.props.numbers, value: "8" },
+        "8"
+      ),
 
+      React.createElement(
+        "button",
+        { id: "nine", onClick: this.props.numbers, value: "9" },
+        "9"
+      ),
 
-      React.createElement("button", {
-        id: "multiply",
-        onClick: this.props.operators,
-        style: operatorStyle,
-        value: "x" }, "x"),
+      React.createElement(
+        "button",
+        {
+          id: "subtract",
+          onClick: this.props.operators,
+          style: operatorStyle,
+          value: "\u2011",
+        },
+        "-"
+      ),
 
+      React.createElement(
+        "button",
+        { id: "four", onClick: this.props.numbers, value: "4" },
+        "4"
+      ),
 
+      React.createElement(
+        "button",
+        { id: "five", onClick: this.props.numbers, value: "5" },
+        "5"
+      ),
 
-      React.createElement("button", { id: "seven", onClick: this.props.numbers, value: "7" }, "7"),
+      React.createElement(
+        "button",
+        { id: "six", onClick: this.props.numbers, value: "6" },
+        "6"
+      ),
 
+      React.createElement(
+        "button",
+        {
+          id: "add",
+          onClick: this.props.operators,
+          style: operatorStyle,
+          value: "+",
+        },
+        "+"
+      ),
 
-      React.createElement("button", { id: "eight", onClick: this.props.numbers, value: "8" }, "8"),
+      React.createElement(
+        "button",
+        { id: "one", onClick: this.props.numbers, value: "1" },
+        "1"
+      ),
 
+      React.createElement(
+        "button",
+        { id: "two", onClick: this.props.numbers, value: "2" },
+        "2"
+      ),
 
-      React.createElement("button", { id: "nine", onClick: this.props.numbers, value: "9" }, "9"),
+      React.createElement(
+        "button",
+        { id: "three", onClick: this.props.numbers, value: "3" },
+        "3"
+      ),
 
+      React.createElement(
+        "button",
+        {
+          className: "jumbo",
+          id: "zero",
+          onClick: this.props.numbers,
+          value: "0",
+        },
+        "0"
+      ),
 
-      React.createElement("button", {
-        id: "subtract",
-        onClick: this.props.operators,
-        style: operatorStyle,
-        value: "\u2011" }, "-"),
+      React.createElement(
+        "button",
+        { id: "decimal", onClick: this.props.decimal, value: "." },
+        "."
+      ),
 
-
-
-      React.createElement("button", { id: "four", onClick: this.props.numbers, value: "4" }, "4"),
-
-
-      React.createElement("button", { id: "five", onClick: this.props.numbers, value: "5" }, "5"),
-
-
-      React.createElement("button", { id: "six", onClick: this.props.numbers, value: "6" }, "6"),
-
-
-      React.createElement("button", {
-        id: "add",
-        onClick: this.props.operators,
-        style: operatorStyle,
-        value: "+" }, "+"),
-
-
-
-      React.createElement("button", { id: "one", onClick: this.props.numbers, value: "1" }, "1"),
-
-
-      React.createElement("button", { id: "two", onClick: this.props.numbers, value: "2" }, "2"),
-
-
-      React.createElement("button", { id: "three", onClick: this.props.numbers, value: "3" }, "3"),
-
-
-      React.createElement("button", {
-        className: "jumbo",
-        id: "zero",
-        onClick: this.props.numbers,
-        value: "0" }, "0"),
-
-
-
-      React.createElement("button", { id: "decimal", onClick: this.props.decimal, value: "." }, "."),
-
-
-      React.createElement("button", {
-        id: "equals",
-        onClick: this.props.evaluate,
-        style: equalsStyle,
-        value: "=" }, "=")));
-
-
-
-
-
-  }}
-
+      React.createElement(
+        "button",
+        {
+          id: "equals",
+          onClick: this.props.evaluate,
+          style: equalsStyle,
+          value: "=",
+        },
+        "="
+      )
+    );
+  }
+}
 
 class Output extends React.Component {
   render() {
-    return (
-      React.createElement("div", { className: "outputScreen", id: "display" },
-      this.props.currentValue));
-
-
-  }}
-
+    return React.createElement(
+      "div",
+      { className: "outputScreen", id: "display" },
+      this.props.currentValue
+    );
+  }
+}
 
 class Formula extends React.Component {
   render() {
-    return React.createElement("div", { className: "formulaScreen" }, this.props.formula);
-  }}
+    return React.createElement(
+      "div",
+      { className: "formulaScreen" },
+      this.props.formula
+    );
+  }
+}
 
-
-ReactDOM.render(React.createElement(Calculator, null), document.getElementById('app'));
+ReactDOM.render(
+  React.createElement(Calculator, null),
+  document.getElementById("app")
+);
